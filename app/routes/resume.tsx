539456dd82bf4b6741291c41662e75df1923ed8b
Stephan van Opstal <stephan@vanopstal.eu>
2024-04-08 22:05:40 +0200

    Content changes

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -5,27 +5,27 @@ export default function Resume() {
         <h2>Experience</h2>
         <h3>Front-end Developer @ Eneco</h3>
         <h4>July 2019 &mdash; March 2024 · Freelance</h4>
-        <p>In my 5 years at Eneco<span className="sidenote-anchor"></span><span
-          className="sidenote">My longest project ever</span>, I was entrusted with the
+        <p><span
+          className="sidenote">My longest project ever</span>In my 5 years at Eneco, I was entrusted with the
           complex but exciting task of migrating an old platform to a
           new one, which heavily relied on <strong>React</strong>, <strong>TypeScript</strong>, <strong>Testing
             Library</strong>. <strong>Sitecore</strong> was used as a CMS.</p>
         <p>I played an important role in elevating the team to the next level. I not only successfully transitioned
           the
           platform with minimal disruption but also spent a significant amount of time improving the way of working.
-          This involved enhancing efficiency in refinement sessions, streamlining peer review processes and
-          writing extensive documententation. Moreover, I played a key role in discussing and implementing data endpoints endpoints.</p>
+          This involved enhancing efficiency in refinement sessions, streamlining the peer review process and
+          writing extensive documentation. Moreover, I played a key role in discussing and implementing data
+          endpoints.</p>
         <p>This experience has sharpened my technical skills and honed my ability to deliver high-quality work under
           tight deadlines.</p>
         <hr/>
         <h3>Front-end Developer @ Bankai</h3>
         <h4>Feb 2019 &mdash; Jun 2019 · Freelance</h4>
-        <p>I helped rebuilding an online platform for trading cars
-          with <strong>React</strong> and <strong>Redux</strong>. Together with another developer we worked under a tight deadline but we delivered.</p>
+        <p>I helped rebuilding an online car-trading platform using <strong>React</strong> and <strong>Redux</strong>. Because of the tight deadline, another front-end developer was brought in. We worked closely together and managed to deliver.</p>
         <hr/>
         <h3>Front-end Developer @ We Are You</h3>
         <h4>Nov 2018 &mdash; Feb 2019 · Freelance</h4>
-        <p>At We Are You, formerly Mangrove, I got to work on the new <em>We Are You</em> branding website.
+        <p>At We Are You, formerly Mangrove, I got to work on the new <em>We Are You</em> brand website.
           An
           animation intense <strong>React</strong> website that required creative solutions for presenting data from
           a <strong>JSS</strong> (Headless
@@ -33,27 +33,28 @@ export default function Resume() {
         <hr/>
         <h3>Front-end Developer @ Iquality / CZ</h3>
         <h4>Mar 2018 &mdash; Dec 2018 · Freelance</h4>
-        <p>Iquality commissioned me to return to CZ<span className="sidenote-anchor"></span><span
-          className="sidenote">My second time at CZ</span> for the second time to help develop the Just.nl
-          website connecting
-          Sitecore datasource with a <strong>React</strong> heavy website.</p>
+        <p><span
+          className="sidenote">My second time at CZ</span>Iquality asked me to return to CZ to help develop the Just.nl
+          website connecting a Sitecore datasource with a <strong>React</strong> heavy front-end. This project involved a lot of planning an discussing ways to solve complex problems.</p>
         <hr/>
         <h3>Front-end Developer @ Redhotminute</h3>
         <h4>Apr 2017 &mdash; Feb 2018 · Freelance</h4>
-        <p>After a good previous collaboration<span className="sidenote-anchor"></span><span
-          className="sidenote">My second time at Redhotminute</span> with Redhotminute I was asked again to help
+        <p><span
+          className="sidenote">My second time at Redhotminute</span> After a good previous collaboration with
+          Redhotminute I was asked again to help
           develop
           two
           new
           projects:</p>
         <ul>
-          <li>Another NGF (Dutch Golf Federation) project. In this case it involved the front-end development for
+          <li>Another NGF (Dutch Golf Federation) project which involved the front-end development for
             NGF's new corporate website. All <strong>React</strong> components were developed
-            using <strong>Patternlab</strong> and implemented in a Sitecore environment using Razor. Other
+            using <strong>Patternlab</strong> and implemented in a <strong>Sitecore</strong> environment
+            using <strong>Razor</strong>. Other
             techniques
             used were <strong>Sass</strong>, <strong>JavaScript</strong> and <strong>Gulp</strong>.
           </li>
-          <li>Rebranding of the VanGansewinkel/Renewi website
+          <li>Rebranding of the VanGansewinkel / Renewi website
             using <strong>Razor</strong>, <strong>Sass</strong> and <strong>JavaScript</strong>.
           </li>
         </ul>
@@ -64,16 +65,14 @@ export default function Resume() {
           styleguide served as the front-end basis in the process of redesigning all <em>CZ</em> portals in a Sitecore
           8.2
           environment.</p>
-        <p>As front-end developer, not only was I responsible for the actual technical implementation in the front-end
-          area, I was also highly involved in coming up with solutions for problems that transcend disciplines, even
+        <p>Not only was I responsible for the actual technical front-end implementations, I was also highly involved in coming up with solutions for cross-discipline problems, even
           before a single line of code was written.</p>
         <hr/>
         <h3>Front-end Developer @ Redhotminute</h3>
         <h4>Feb 2016 &mdash; Jul 2016 · Freelance</h4>
         <p>I helped develop an app with accompanying website for the <em>NGF</em> (Dutch Golf Federation). My
-          responsibility was mainly with the front-end for the responsive website. I worked closely with the designer
-          and the Sitecore/iOS developers because the main focus of the design was on the app and the website had to
-          match that as closely as possible. Long-term project of several months in a SCRUM team. Some of the
+          responsibility was mainly with the front-end for the responsive website. I worked closely with designers
+          and Sitecore/iOS developers because it was important for the app and website to be as similar as possible. Some of the
           techniques
           used: <strong>React</strong>, <strong>Sass</strong>, <strong>Webpack</strong>, <strong>Gulp</strong>, <strong>Sketch</strong>.
         </p>
@@ -83,24 +82,23 @@ export default function Resume() {
         <p>I worked on the iOS/Android/Windows app with accompanying website and CMS system. The hybrid app
           (<strong>HTML5</strong>/<strong>CSS</strong>/<strong>JavaScript</strong>) was built on the Cordova
           framework.
-          The back-end was built in <strong>PHP</strong> using Twig as the template engine. Besides
-          writing <strong>HTML</strong>,
-          <strong>CSS</strong> and <strong>JavaScript</strong>, I was also responsible
-          for <strong>UX/UI</strong> related challenges which were numerous in this project.</p>
+          The back-end was built in <strong>PHP</strong> using Twig as the template engine. I was also responsible
+          for coming up with solutions for <strong>UX/UI</strong> related challenges, which were numerous in this project.</p>
         <hr/>
-        <h3>Front-end Developer  @ Online Department</h3>
+        <h3>Front-end Developer @ Online Department</h3>
         <h4>Oct 2014 &mdash; Oct 2014 · Freelance</h4>
         <p>Short assignment as a supporting front-end developer in the realization of a Wordpress site.</p>
         <hr/>
-        <h3>Front-end Developer  @ Netvlies</h3>
+        <h3>Front-end Developer @ Netvlies</h3>
         <h4>Jun 2014 &mdash; Oct 2014 · Freelance</h4>
-        <p>My responsibility during this job was mainly in the front-end development of a responsive webshop for people
+        <p><span className="sidenote">My third time @ Netvlies</span>My responsibility during this job was mainly in
+          the front-end development of a responsive webshop for people
           with physical disabilities. Implementation of the templates I created were done in a Symfony (PHP) back-end
           using Twig templates. Extensive project with a large number of templates. Some of the techniques
           used: <strong>HTML</strong>, <strong>Sass</strong>, <strong>jQuery</strong>, <strong>Grunt</strong>, <strong>PHP</strong>.
         </p>
         <hr/>
-        <h3>Front-end Developer  @ Infostrada</h3>
+        <h3>Front-end Developer @ Infostrada</h3>
         <h4>Dec 2013 &mdash; May 2014 · Freelance</h4>
         <p>I developed the front-end for Videoland.com, a full responsive website with a large number of templates.
           Designs were provided and I converted them to <strong>HTML</strong>, <strong>CSS
@@ -108,48 +106,56 @@ export default function Resume() {
           for the back-end developers.
         </p>
         <hr/>
-        <h3>Front-end Developer  @ Netvlies</h3>
+        <h3>Front-end Developer @ Netvlies</h3>
         <h4>Oct 2013 &mdash; Nov 2013 · Freelance</h4>
-        <p>I was asked to develop the front-end of the ATOS IT Challenge website.</p>
+        <p><span className="sidenote">My second time @ Netvlies</span> I was asked to develop the front-end of the ATOS IT Challenge website.</p>
         <hr/>
-        <h3>Front-end Developer  @ VI Company</h3>
+        <h3>Front-end Developer @ VI Company</h3>
         <h4>Aug 2013 &mdash; Sep 2013 · Freelance</h4>
-        <p>Back at VI Company for the third time where I helped out on multiple running projects. Within VI Company
-          the <strong>BackboneJS</strong> framework was used in combination with a <strong>.NET</strong>
-          back-end</p>
+        <p><span className="sidenote">My third time @ VI Company</span> I
+          helped out on multiple running projects. Within VI Company
+          the <strong>BackboneJS</strong> framework was used in combination with a <strong>.NET</strong> back-end.</p>
         <hr/>
-        <h3>Front-end Developer  @ Colours</h3>
+        <h3>Front-end Developer @ Colours</h3>
         <h4>Jul 2013 &mdash; Jul 2013 · Freelance</h4>
+        <p>Small project where I created some front-end templates based on provided designs.</p>
         <hr/>
-        <h3>Front-end Developer  @ Hemels van der Hart</h3>
+        <h3>Front-end Developer @ Hemels van der Hart</h3>
         <h4>Jun 2013 &mdash; Jul 2013 · Freelance</h4>
+        <p><span className="sidenote">Second time @ Hemels van der Hart</span> I put together multiple front-end
+          templates for tiny promotional websites
+          using <strong>HTML</strong>, <strong>CSS</strong> and <strong>JavaScript</strong>.</p>
         <hr/>
-        <h3>Front-end Developer  @ Netvlies</h3>
+        <h3>Front-end Developer @ Netvlies</h3>
         <h4>May 2013 &mdash; Jun 2013 · Freelance</h4>
+        <p><span className="sidenote">Back @ the old nest</span> I helped out on multiple running projects.</p>
         <hr/>
-        <h3>Front-end Developer  @ Hemels van der Hart</h3>
+        <h3>Front-end Developer @ Hemels van der Hart</h3>
         <h4>Feb 2013 &mdash; Mar 2013 · Freelance</h4>
+        <p>I put together multiple front-end templates for tiny promotional websites
+          using <strong>HTML</strong>, <strong>CSS</strong> and <strong>JavaScript</strong>.</p>
         <hr/>
-        <h3>Front-end Developer  @ Thingks</h3>
+        <h3>Front-end Developer @ Thingks</h3>
         <h4>Feb 2013 &mdash; Feb 2013 · Freelance</h4>
+        <p>Short project where I created the front-end for a promotional website
+          using <strong>HTML</strong>, <strong>CSS</strong> and <strong>JavaScript</strong>.</p>
         <hr/>
-        <h3>Front-end Developer  @ VI Company</h3>
+        <h3>Front-end Developer @ VI Company</h3>
         <h4>Oct 2012 &mdash; Dec 2012 · Freelance</h4>
-        <p>Back at VI Company for the second time where I helped out on multiple running projects. Within VI Company
-          the <strong>BackboneJS</strong> framework was used in combination with a <strong>.NET</strong>
-          back-end</p>
+        <p><span className="sidenote">My second time @ VI Company</span>I helped out on multiple running projects. Within VI Company
+          the <strong>BackboneJS</strong> framework was used in combination with a <strong>.NET</strong> back-end.</p>
         <hr/>
-        <h3>Front-end Developer  @ VI Company</h3>
+        <h3>Front-end Developer @ VI Company</h3>
         <h4>Jun 2012 &mdash; Aug 2012 · Freelance</h4>
-        <p>In my role as front-end developer in a SCRUM team I worked closely
-          with the designers and back-end developers. I mainly focussed on projects for ING Germany that were related to
-          the financial world. Within VI Company the BackboneJS framework was used in combination with a .NET
+        <p>While participating in a SCRUM team I worked closely
+          with designers and back-end developers. I mainly focused on projects for ING Germany that were related to
+          the financial world. Within VI Company the <strong>BackboneJS</strong> framework was used in combination with a .NET
           back-end.</p>
         <hr/>
-        <h3>Front-end Developer  @ Facetbase</h3>
+        <h3>Front-end Developer @ Facetbase</h3>
         <h4>Mar 2012 &mdash; Jun 2012 · Freelance</h4>
-        <p>As a freelance front-end web developer, I developed templates for their product Annual Reports. Facetbase at
-          the time had clients such as: Rabobank, Brabant Water, Van Gansewinkel and Oxfam Novib.</p>
+        <p><span className="sidenote">My first job as a freelancer</span> I
+          developed <strong>HTML</strong> and <strong>CSS</strong> templates for Facetbase&apos;s Annual Reports product. They worked for clients such as: Rabobank, Brabant Water, Van Gansewinkel and Oxfam Novib.</p>
         <hr/>
         <h3>Lead Front-end Developer @ Netvlies</h3>
         <h4>Nov 2011 &mdash; Feb 2012 · Full-time</h4>
@@ -157,6 +163,7 @@ export default function Resume() {
         <h4>Feb 2006 &mdash; Nov 2011 · Full-time</h4>
         <h3> Front-end Developer @ Netvlies</h3>
         <h4>Sep 2005 &mdash; Feb 2006 · Internship</h4>
+        <p>Where it all began.</p>
       </section>
       <section>
         <h2>Education</h2>
